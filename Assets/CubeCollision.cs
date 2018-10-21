7a01a699f84ae88d23cc93dfcba0e5ca0b3a42a1
wangw3939 <44263751+wangw3939@users.noreply.github.com>
2018-10-21 22:26:07 +0100

    upload20181021 22:26

---
+++ Assets/CubeCollision.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CubeCollision : MonoBehaviour {
+
+    public CubeMovement movement;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.GetComponent<Collider>().tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+}
