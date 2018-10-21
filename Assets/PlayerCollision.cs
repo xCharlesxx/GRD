acebd1e536de628e5389b1229cfb2b84b4c04a5d
Nshan Petrosyan <nshanpetrosyann@gmail.com>
2018-10-21 16:48:55 +0200

    How to make a Video Game in Unity - COLLISION (E05)
    Created collision with another object(wall)

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+    public playermovement movement;
+void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);
+        if (collisionInfo.collider.tag == "wall")
+        {
+            movement.enabled = false;
+        }
+    }
+}
