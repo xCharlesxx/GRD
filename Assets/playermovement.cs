ec1ce2a54aea217425a43696e6e1e208023cd648
Nshan Petrosyan <nshanpetrosyann@gmail.com>
2018-10-21 16:18:09 +0200

    How to make a Video Game in Unity - MOVEMENT (E03)
    Making the player move and creating the speed in which it goes forward and having input on keys 'a' and 'd' to move sideways

--- Assets/playermovement.cs
+++ Assets/playermovement.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class playermovement : MonoBehaviour {
     public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewwaysForce = 500f;
 	// Use this for initialization
 	
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        rb.AddForce(0, 0, 2000 * Time.deltaTime);
-	}
+        //Add a forward force
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewwaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewwaysForce * Time.deltaTime, 0, 0);
+        }
+    }
 }
