8de84e73adfb486b6d77730dee5bb29b10edde31
Nshan Petrosyan <nshanpetrosyann@gmail.com>
2018-10-21 15:49:08 +0200

    How to make a Video Game in Unity - PROGRAMMING (E02)
    Part 2 of the tutorial where we make an object constantly move to a direction

---
+++ Assets/playermovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour {
+    public Rigidbody rb;
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddForce(0, 0, 2000 * Time.deltaTime);
+	}
+}
